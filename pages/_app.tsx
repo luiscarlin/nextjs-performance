7bab5aeabcebc66b1fa94e9909ff4f3eb59bec7b
Luis Carlin <luiscarlin@gmail.com>
2022-08-14 07:52:37 -0400

    add wdyr library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import '../scripts/wdyr';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
